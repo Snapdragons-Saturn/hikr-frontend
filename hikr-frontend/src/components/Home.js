df788bec71a1927f4cc5f6b4bbb8fab29ad53047
iyad-abdulwahid <iyadabdulwahid23@gmail.com>
2022-08-04 22:45:55 +0000

    added image place holders

--- hikr-frontend/src/components/Home.js
+++ hikr-frontend/src/components/Home.js
@@ -3,8 +3,31 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <div>
+                <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="hike" className="region_image" />
+                <div>
+                    <h4>Hike/Region</h4>
+                    <p>Hike/Region description</p>
+                </div>
+            </div>
+
+                <div>
+                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"  alt="hike" className="region_image" />
+                    <div>
+                        <h4>Hike/Region</h4>
+                        <p>Hike/Region description</p>
+                    </div>
+                </div>
+
+                <div>
+                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"  alt="hike" className="region_image" />
+                    <div>
+                        <h4>Hike/Region</h4>
+                        <p>Hike/Region description</p>
+                    </div>
+                </div>
         </div>
+        
     );
 };
 
