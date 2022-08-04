e9ba68f2ca9d987786c44dd6c1597b5f323537cf
thummel1867 <thummel1867@gmail.com>
2022-08-04 19:02:05 -0400

    starts to build out axios frontend

--- hikr-frontend/src/components/Region.js
+++ hikr-frontend/src/components/Region.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 
 const Region = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  const { name } = useParams;
+  const [region, setRegion] = useState();
+  const [edit, setEdit] = useState(false);
 
+  useEffect(() => {
+    axios.get(``).then((res) => {
+      setRegion(res.data);
+    });
+  }, [name]);
+
+  const showEdit = () => {
+    setEdit(false)
+  }
+
+  const showRegions = () => {
+    setEdit(true)
+  }
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    axios.delete(``, region);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.put(``, region)
+    };
+
+  let regionList = region.map((e) => {
+    return (
+      <div className="regionMap" key="">
+        
+      </div>
+    );
+  });
+{edit ? (
+  <div>{regionList}</div>
+) : (
+    <div>
+
+    </div>
+)
+}
+}
 export default Region;
+
+//
