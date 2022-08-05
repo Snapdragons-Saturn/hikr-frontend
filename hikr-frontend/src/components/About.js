91c09d411a4060646090a2f48a3e7b3df5ab6ee4
iyad-abdulwahid <iyadabdulwahid23@gmail.com>
2022-08-05 03:47:57 +0000

    Added to about and added a background picture

--- hikr-frontend/src/components/About.js
+++ hikr-frontend/src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className='biggest_div'>
             <h1>
                 <em>
                 WELCOME TO HIKR!
