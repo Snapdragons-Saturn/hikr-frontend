31414861811b819287598338c6ad40f9c06545d3
thummel1867 <thummel1867@gmail.com>
2022-08-05 10:51:01 -0400

    flushes out home component

--- hikr-frontend/src/components/Home.js
+++ hikr-frontend/src/components/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {Link} from 'react-router-dom'
 
 const Home = () => {
+const regions = ['']
+
+  let regionList = regions.map((e) => {
     return (
-        <div>
-            
-        </div>
+      <div className="regionMap" key="">
+        <p>
+          <Link
+            className="c"
+            to={"/region/" + e}
+          >
+            <img 
+                src =''
+                alt=''
+            />
+          </Link>
+          <h2>{e}</h2>
+        </p>
+      </div>
     );
+  });
+  return <div>{regionList}</div>;
 };
 
 export default Home;
--- hikr-frontend/src/components/Region.js
+++ hikr-frontend/src/components/Region.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-
 const Region = () => {
   const { name } = useParams;
   const [region, setRegion] = useState();
@@ -15,12 +14,12 @@ const Region = () => {
   }, [name]);
 
   const showEdit = () => {
-    setEdit(false)
-  }
+    setEdit(false);
+  };
 
   const showRegions = () => {
-    setEdit(true)
-  }
+    setEdit(true);
+  };
 
   const handleDelete = (event) => {
     event.preventDefault();
@@ -29,25 +28,35 @@ const Region = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.put(``, region)
-    };
+    axios.put(``, region);
+  };
 
   let regionList = region.map((e) => {
     return (
+        //display region info below
       <div className="regionMap" key="">
-        
+        {e.name}
+        {}
+        <img src="" alt="" />
+        <button onClick = {showEdit}>Edit Hike</button>
       </div>
     );
   });
-{edit ? (
-  <div>{regionList}</div>
-) : (
+  // eslint-disable-next-line no-lone-blocks
+  {
+    edit ? (<div>{regionList}</div> 
+    ) : ( 
     <div>
-
+        
     </div>
-)
-}
-}
+    )
+  }
+};
 export default Region;
 
-//
+//ok, what do I do here?
+//need to grab the hikes from the database and set them to state
+//for each one need to create a div with a description of the hike, possibly a picture?
+//each one needs to have an edit button, when the edit button is clicked it brings up the edit page
+//the edit page will have a button that allows users to delete the hike, and a form to edit a specific portion of the hike
+//the bottom of the page will have a link that brings people to a add hike page
