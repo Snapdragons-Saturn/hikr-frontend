ec77b6d82bc8322a2a25684d8216c37526c90444
thummel1867 <thummel1867@gmail.com>
2022-08-07 12:47:00 -0400

    updates home component with links to regions

--- hikr-frontend/src/components/Home.js
+++ hikr-frontend/src/components/Home.js
@@ -1,43 +1,62 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Home = () => {
-// const regions = {}
+  // const regions = {}
 
-    return (
-        <div className='big_div'>
-            <div className='img1'>
-                <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="hike" className="region_image" />
-            </div>
-                <div className='img_text1'>
-                    <h4 className='text'>Hike/Region</h4>
-                </div>
+  return (
+    <div className="big_div">
+      <Link to="/region/NE" className="img1">
+        <img
+          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="hike"
+          className="region_image"
+        />
+      </Link>
+      
 
-                <div className='img2'>
-                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"  alt="hike" className="region_image" />
-                </div>
-                    <div className='img_text2'>
-                        <h4 className='text'>Hike/Region</h4>
-                    </div>
+      <Link to="/region/SA" className="img2">
+        <img
+          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="hike"
+          className="region_image"
+        />
+      </Link>
 
-                <div className='img3'>
-                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"  alt="hike" className="region_image" />
-                </div>
-                    <div className='img_text3'>
-                        <h4 className='text'>Hike/Region</h4>
-                    </div>
-                <div className='img4'>
-                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"  alt="hike" className="region_image" />
-                </div>
-                    <div className='img_text4'>
-                        <h4 className='text'>Hike/Region</h4>
-                    </div>
-        </div>
-        
-    );
-  }
-  
+
+      <Link to="region/MW" className="img3">
+        <img
+          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="hike"
+          className="region_image"
+        />
+      </Link>
+      
+      <Link to="/region/SC" className="img4">
+        <img
+          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="hike"
+          className="region_image"
+        />
+      </Link>
+
+      <Link to="/region/M" className="img4">
+        <img
+          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="hike"
+          className="region_image"
+        />
+      </Link>
+      <Link to="/region/PC" className="img4">
+        <img
+          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="hike"
+          className="region_image"
+        />
+      </Link>
+    </div>
+  );
+};
+
 export default Home;
-
-
