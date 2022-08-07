3165e1c2527afc568d580e62ec0cb375b02eb1e2
thummel1867 <thummel1867@gmail.com>
2022-08-07 14:23:47 -0400

    rebase

--- hikr-frontend/src/components/Home.js
+++ hikr-frontend/src/components/Home.js
@@ -1,61 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import list from '../list.json'
 
 const Home = () => {
   // const regions = {}
 
-  return (
-    <div className="big_div">
-      <Link to="/region/NE" className="img1">
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt="hike"
-          className="region_image"
-        />
-      </Link>
-      
+  let regionList =
+  list.map(e => 
+    {
+    return (
+      <div className="countryList" key={e.id}>
+        <p>
+          <Link className="countryLinks" to={"/regions/" + e.region}> {e.name}</Link>
+        </p>
+      </div>
+  )});
+  return <div>{regionList}</div>;
+  };
 
-      <Link to="/region/SA" className="img2">
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt="hike"
-          className="region_image"
-        />
-      </Link>
 
-      <Link to="region/MW" className="img3">
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt="hike"
-          className="region_image"
-        />
-      </Link>
-      
-      <Link to="/region/SC" className="img4">
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt="hike"
-          className="region_image"
-        />
-      </Link>
-
-      <Link to="/region/M" className="img4">
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt="hike"
-          className="region_image"
-        />
-      </Link>
-      <Link to="/region/PC" className="img4">
-        <img
-          src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt="hike"
-          className="region_image"
-        />
-      </Link>
-    </div>
-  );
-};
 
 export default Home;
