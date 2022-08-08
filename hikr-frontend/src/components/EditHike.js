2400323a5f6ab6a68017ae494bef8a29fdf83fe4
thummel1867 <thummel1867@gmail.com>
2022-08-08 13:23:56 -0400

    updates edithikes component

--- hikr-frontend/src/components/EditHike.js
+++ hikr-frontend/src/components/EditHike.js
@@ -24,7 +24,10 @@ const EditHike = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.put(`https://desolate-ocean-19551.herokuapp.com/api/hikes/${_id}`, region);
+    axios.put(`https://desolate-ocean-19551.herokuapp.com/api/hikes/${_id}`, region).then(() => {
+      navigate(`/hike/${_id}`)
+    })
+    
   };
 
   const handleChange = (event) => {
@@ -36,8 +39,52 @@ const EditHike = () => {
   return <div>
     <h2>Editing {hike.hikeName}</h2>
     <form onSumbit={handleSubmit}>
-        <label></label>
-        <input onChange={handleChange}></input>
+        <label>State: </label>
+        <input 
+        id = "stateAbb"
+        type="text"
+        defaultValue={hike.stateAbb}
+        onChange={handleChange}>
+        </input>
+        <label>Hike Name: </label>
+        <input
+        id = "hikeName" 
+        type="text"
+        defaultValue={hike.hikeName}
+        onChange={handleChange}>
+        </input>
+        <label>Hike Difficulty: </label>
+        <input 
+        id="hikeDiff"
+        type="text"
+        defaultValue={hike.hikeDiff}
+        onChange={handleChange}>
+        </input>
+
+        <label>Hike Distance: </label>
+        <input 
+        id="hikeLen"
+        type="text"
+        defaultValue={hike.hikeLen}
+        onChange={handleChange}>
+        </input>
+
+        <label>Hike Terrain: </label>
+        <input 
+        id="hikeTerrain"
+        type="text"
+        defaultValue={hike.hikeTerrain}
+        onChange={handleChange}>
+        </input>
+
+
+        <label>Hike Description: </label>
+        <input 
+        id="hikeDesc"
+        type="text"
+        defaultValue={hike.hikeDesc}
+        onChange={handleChange}>
+        </input>
     </form>
     <button onClick={handleDelete}>Delete Hike</button>
   </div>;
