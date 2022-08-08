0a3d8943cb9dd34e451e37df033759a3a30cf9cf
thummel1867 <thummel1867@gmail.com>
2022-08-08 11:39:32 -0400

    begins to build addhike component

--- hikr-frontend/src/components/AddHike.js
+++ hikr-frontend/src/components/AddHike.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const AddHike = () => {
+    const [hike, setHike] = useState([])
+
+    const addHikes = () => {
+        axios.post(`https://desolate-ocean-19551.herokuapp.com/api/hikes/`).then((res) => {
+          console.log(res.data)
+            setHike(res.data);
+          });
+      }
+
     return (
         <div>
             
