9b5608c465dc9277af5bceef893e88366c181cc9
bellamalo1 <bmaldo23@gmail.com>
2022-08-09 11:46:02 -0700

    sign-up form basics

--- hikr-frontend/src/components/Login.js
+++ hikr-frontend/src/components/Login.js
@@ -3,8 +3,13 @@ import {Routes, Route, Link} from "react-router-dom"
 
 
 function Login (){
+
+
+    
     return(
         <div className="loginPage">
+
+
             <Link className="signUpLink" to='/Sign-Up'>
                 <h1>Sign-Up</h1>
             </Link>
